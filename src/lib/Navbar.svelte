b63b6c556b430fa3e84cd56ff64f7f30d2c4739f
Quang Nguyen <s3927198@rmit.edu.vn>
2023-02-13 11:02:32 +0700

    Finish landing page

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,45 @@
+<nav>
+    <div class="brand">What</div>
+    <ul>
+        <li><a href="#">Menu</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Team</a></li>
+    </ul>
+    <button>Open</button>
+</nav>
+
+<style>
+    @font-face {
+        font-family: "Valkocapela";
+        src: url("./src/assets/VALKOCAPELA.woff");
+    }
+    * {
+        font-family: "Valkocapela";
+    }
+    nav {
+        position: absolute;
+        top: 3vh;
+        left: 3vw;
+        display: flex;
+        width: 91vw;
+        justify-content: space-between;
+        align-items: flex-start;
+        z-index: 3;
+    }
+    nav > ul {
+        display: flex;
+        min-width: 30vw;
+        justify-content: space-evenly;
+    }
+    nav li {
+        list-style-type: none;
+    }
+    nav li > a {
+        color: white;
+        text-decoration: none;
+    }
+    .brand {
+        color: white;
+        font-size: xx-large;
+    }
+</style>
