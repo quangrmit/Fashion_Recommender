e3a11ae0440c29e35e8f0b66a420eb46a9023312
Phu Mai <s3927049@rmit.edu.vn>
2023-03-05 22:41:16 +0700

    add suggest page

---
+++ src/lib/Suggest.svelte
@@ -0,0 +1,32 @@
+<script>
+
+import axios from 'axios';
+
+let prompt;
+let answer;
+
+async function sendMessage(prompt) {
+  const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+    prompt: prompt,
+    max_tokens: 50,
+    temperature: 0.5,
+    n: 1,
+
+
+
+  }, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer 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'
+    }
+  });
+  console.log(response.data.choices[0].text);
+  answer = response.data.choices[0].text;
+  return response.data.choices[0].text;
+}
+
+</script>
+
+<input bind:value={prompt}>
+<button type="button" name="button" on:click={() => sendMessage(prompt)}>click</button>
+<p>{answer}</p>
---
+++ src/suggest.js
@@ -0,0 +1,8 @@
+import "./app.css";
+import Suggest from "./lib/Suggest.svelte";
+
+const suggest = new Suggest({
+    target: document.getElementById("suggest"),
+});
+
+export default suggest;
